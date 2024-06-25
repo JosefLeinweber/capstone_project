b2370d26c4c1e5f4c7237d4e315b0f51b31a78b9
Josef Leinweber <josef.leinweber@code.berlin>
2024-06-25 11:41:05 +0200

    savetfy commit before making structural changes

--- src/audioBuffer/AudioBuffer.h
+++ src/audioBuffer/AudioBuffer.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_core/juce_core.h>
 #include <juce_events/juce_events.h>
 
